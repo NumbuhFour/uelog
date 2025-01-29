606a7ba37a2129e41d61aafcb67d52bc7f4099ee
CodeCartographer <billythegoat101@gmail.com>
2025-01-28 19:31:03 -0800

    Move version to package

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,3 @@
-
-export const AppVersion = 0.1;
-export const FileVersion = 1.0;
+const { version, fileformatVersion } = require('../package.json')
+export const AppVersion = version;
+export const FileVersion = fileformatVersion;
